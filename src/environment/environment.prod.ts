a80c143077ba4b9d3ef2a4bf999f47ab1404a41f
aayushclavrit <aayush@clavrit.com>
2024-06-24 22:29:20 +0530

    fourth commit

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://52.66.211.139:4200/angularekart',
+};
---
+++ src/environment/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  apiUrl: 'http://52.66.211.139:4200/angularekart',
+};
